d5b09475ba0548ad0e6dcf0dabd3d6d440331b9c
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-10 12:26:50 +0200

    multiple inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,9 +8,19 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("empty");
+    }
   };
   return (
     <>
@@ -39,6 +49,15 @@ const ControlledInputs = () => {
           </div>
           <button type="submit">add person</button>
         </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
